31551631e7169d7cfd0c8815df1b66870500e2f4
Philipp Schellhaass <schrecktierbergli@gmail.com>
2025-08-03 13:35:56 +0200

    Skalierungsfehler des settings_button behoben

--- app/main.py
+++ app/main.py
@@ -2,11 +2,24 @@ from ursina import *
 
 
 def main_menu():
-    start_button = Button(text="Start", parent=camera.ui,
-                          position=(0, -0.35), color=rgb(112/255, 146/255, 190/255), scale=(0.35, 0.125), text_size=2, text_origin=(0, -0.05))
+    start_button = Button(
+        text="Start",
+        parent=camera.ui,
+        position=(0, -0.35),
+        color=rgb(112/255, 146/255, 190/255),
+        scale=(0.35, 0.125),
+        text_size=2,
+        text_origin=(0, -0.05))
 
-    settings_button = Button(text="Einstellungen", parent=start_button, position=(
-        1.5, 0), color=rgb(112/255, 146/255, 190/255), scale=(1, 1))
+    settings_button = Button(
+        text="Einstellungen",
+        parent=camera.ui,  # <-- geändert
+        position=(0.55, -0.35),  # eigene Position
+        color=rgb(112/255, 146/255, 190/255),
+        scale=(0.35, 0.125),  # gleiche Skalierung wie Start-Button
+        text_size=1.5,
+        text_origin=(0, -0.01)
+    )
 
 
 app = Ursina()
