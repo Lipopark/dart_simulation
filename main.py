d418df6e7e8b12b42ced9407463881f6f2ee8c30
Philipp Schellhaass <schrecktierbergli@gmail.com>
2025-08-11 19:52:17 +0200

    Es kann nun zwischen zwei Dartpfeilmodellen gewechselt werden

--- main.py
+++ main.py
@@ -52,22 +52,55 @@ def main_menu():
 
     dart2 = Entity(
         model=r"models_compressed\dart2\dart2.obj",
-        # model=r"models_compressed\dart1\dart2.obj",
         scale=(100),
         rotation=(-10, 25, 20),
-        position=(1, 0.2, 0)
+        position=(7, 0.2, 0)
     )
+
+    global darts_list
+    darts_list = [dart1, dart2]
+    darts_list[0]
+    switch_l_button.collision = False
+
+    def switch_r():
+        global selected_dart
+        global x
+        x += 1
+        selected_dart.position = -10, 0.2, 0
+        selected_dart = darts_list[x]
+        selected_dart.position = 0, 0.2, 0
+        switch_l_button.collision = True
+        if x == 1:
+            switch_r_button.collision = False
+
+    def switch_l():
+        global selected_dart
+        global x
+        x -= 1
+        selected_dart.position = 7, 0.2, 0
+        selected_dart = darts_list[x]
+        selected_dart.position = 0, 0.2, 0
+        switch_r_button.collision = True
+        if x == 0:
+            switch_l_button.collision = False
+
+    switch_r_button.on_click = switch_r
+    switch_l_button.on_click = switch_l
 
 
 app = Ursina(
     title="Dartwurf Simulation"
 )
 
+
+x = 0
 Entity.default_shader = lit_with_shadows_shader
 Text.default_font = r"fonts\arial_unicode_ms_bold.otf"
 Text.resolution = 200
 EditorCamera()
 
+
 main_menu()
+selected_dart = darts_list[0]
 
 app.run()
