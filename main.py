df7c322b311107fd63c33f64d2ec2d25a4eeee84
Philipp Schellhaass <schrecktierbergli@gmail.com>
2025-08-22 12:56:25 +0200

    Neue Buttons hinzugefügt, sowie Animation überarbeitet, aber nicht fertiggestellt.

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from ursina.shaders import *
 
 def main_menu():
     global darts_list
-
     # Ist der erste Dart ausgewählt, wird der Button nach links deaktiviert
     if x == 0:
         switch_l_button.collision = False
-
+    else:
+        switch_l_button.collision = True
     for y in main_menu_entities:
         y.enabled = True
 
     # Beim Start wird standartmässig der erste Dart angezeigt
     darts_list[0]
+
+
+def settings():
+    # Alle Menu Entities werden deaktiviert
+    for y in main_menu_entities:
+        y.enabled = False
+    # Alle Settings Entities werden aktiviert
+    for y in settings_entities:
+        y.enabled = True
 
 
 def switch_r():
@@ -42,9 +51,19 @@ def switch_l():
         switch_l_button.collision = False
 
 
+def camera_position_r():
+    pass
+
+
+def camera_position_l():
+    pass
+
+
 def back_to_menu():
     # Alles wird zurückgesetzt, damit es wieder wie im Hauptmenü aussieht. Der ausgewählte Dart bleibt aber ausgewählt
     back_to_menu_button.enabled = False
+    for y in settings_entities:
+        y.enabled = False
     board.enabled = False
     selected_dart.scale = 100
     selected_dart.position = (0, 0.2, 0)
@@ -60,27 +79,54 @@ def start():
 
     # Der ausgewählte Dart wird in Startposition gebracht
     selected_dart.scale = 1
-    selected_dart.position = (0.12, 0, -20)
-    selected_dart.rotation = (67, 176.9, 0)
+    selected_dart.position = (0.12, 0, -19)
+    selected_dart.rotation = (45, 176.9, 0)
     selected_dart.enabled = True
-    selected_dart.x_rot_speed = 10
 
     # Board wird angezeigt
     board.enabled = True
 
     # Animation startet
-    def update():
+
+    """def update():
+        pass
         selected_dart.position -= selected_dart.forward / 9.35
         selected_dart.rotation_x -= selected_dart.x_rot_speed
 
         # Übergang von Phase 1 des Wurfs in Phase 2 wird überprüft
         if selected_dart.rotation_x <= 13:
-            selected_dart.x_rot_speed = 2
+            selected_dart.x_rot_speed = 1.5
 
         # Bei Kollision mit dem Board wird die Update Funktion beendet
         if selected_dart.intersects(board):
             selected_dart.update = False
+            back_to_menu_button.enabled = True"""
+
+    selected_dart.x_rot_speed = 10
+    selected_dart.a = 0.01
+
+    def update():
+        # Kollision mit Board ca. bei selected_dart.x = 0.0953
+        if selected_dart.intersects(board):
+            selected_dart.update = False
             back_to_menu_button.enabled = True
+        else:
+            # Übergang von Phase 1 des Wurfs in Phase 2 wird überprüft
+            # if selected_dart.rotation_x >= 13:
+
+            if selected_dart.z <= -18.7:
+                # Phase 1
+                selected_dart.position -= selected_dart.forward / 9.35
+                selected_dart.rotation_x -= selected_dart.x_rot_speed
+
+            else:
+                # Phase 2
+                selected_dart.z += 0.1
+                selected_dart.y += selected_dart.a
+                selected_dart.a -= 0.0005
+                selected_dart.rotation_x += 1
+
+                # selected_dart.rotation_x -= selected_dart.x_rot_speed
 
     selected_dart.update = update
 
@@ -89,11 +135,12 @@ app = Ursina(
     title="Dartwurf Simulation"
 )
 
+camera.position = 0, 0, -19.4
 # Shader und die Schriftanzeige werden gesetzt
 Entity.default_shader = lit_with_shadows_shader
 Text.default_font = r"fonts\arial_unicode_ms_bold.otf"
 Text.resolution = 200
-EditorCamera()
+# EditorCamera()
 window.fullscreen = True
 
 color_buttons = rgb(112/255, 146/255, 190/255)
@@ -119,6 +166,20 @@ settings_button = Button(
     text_origin=(0, -0.01)
 )
 
+settings_button.on_click = settings
+
+quit_button = Button(
+    text="Schliessen",
+    parent=camera.ui,
+    position=(-0.55, -0.35),
+    color=color_buttons,
+    scale=(0.35, 0.125),
+    text_size=1.5,
+    text_origin=(0, -0.01)
+)
+
+quit_button.on_click = quit
+
 switch_r_button = Button(
     text="▶",
     parent=camera.ui,
@@ -143,6 +204,55 @@ switch_l_button = Button(
 
 switch_l_button.on_click = switch_l
 
+settings_title = Text(
+    text="Einstellungen",
+    parent=camera.ui,
+    position=(0, 0.4),
+    scale=2,
+    origin=(0, 0),
+
+)
+
+camera_position_title = Text(
+    text="Kameraperspektive:",
+    parent=camera.ui,
+    position=(-0.2, 0.32),
+    scale=1,
+    origin=(0, 0.5),
+)
+
+camera_position_r_button = Button(
+    text="\n▶\n",
+    parent=camera.ui,
+    position=(0.3, 0.31),
+    color=color_buttons,
+    scale=(0.05),
+    text_size=2,
+    text_origin=(0, -0.4)
+)
+
+camera_position_r_button.on_click = camera_position_r
+
+camera_position_l_button = Button(
+    text="\n◀\n",
+    parent=camera.ui,
+    position=(0.06, 0.31),
+    color=color_buttons,
+    scale=(0.05),
+    text_size=2,
+    text_origin=(0, -0.4)
+)
+
+camera_position_text = Text(
+    text="Frontal",
+    parent=camera.ui,
+    position=(0.18, 0.32),
+    scale=1,
+    origin=(0, 0.5),
+)
+
+camera_position_l_button.on_click = camera_position_l
+
 back_to_menu_button = Button(
     text="Zurück zum\nHauptmenü",
     parent=camera.ui,
@@ -153,15 +263,15 @@ back_to_menu_button = Button(
     text_origin=(0, -0.01),
 )
 
-back_to_menu_button.enabled = False
 back_to_menu_button.on_click = back_to_menu
 
 board = Entity(
     model=r"models_compressed\board\board.obj",
+    # model=r"models_compressed\board\board_distance.obj",
     texture=r"\models\board\board_picture.jpg",
     scale=(1),
     rotation=(0, 90, 0),
-    position=(0, 0, -17.74),
+    position=(0, 0, -16.74),
     collider="box"
 )
 
@@ -183,12 +293,49 @@ dart2 = Entity(
     collider="box"
 )
 
+# Anzeigeeinstellung
+
+
+def input(key):
+    if key == "t":
+        print_on_screen("rotate")
+        camera.rotation_y += 90
+
+    if key == "p":
+        print_on_screen(camera.position)
+
+    if key == "w":
+        camera.position += camera.forward
+
+    if key == "s":
+        camera.position += camera.back
+
+    if key == "a":
+        camera.position += camera.left
+
+    if key == "d":
+        camera.position += camera.right
+
+    if key == "k":
+        camera.position = (-4, 0, -17.8)
+        camera.rotation = (0, 90, 0)
+
+    if key == "r":
+        camera.position = (0, 0, -19.4)
+        camera.rotation = (0, 0, 0)
+
+
 x = 0
+
 darts_list = [dart1, dart2]
 main_menu_entities = [start_button, settings_button,
-                      switch_r_button, switch_l_button]
+                      quit_button, switch_r_button, switch_l_button]
 main_menu_entities += darts_list
 
+settings_entities = [settings_title, camera_position_r_button,
+                     camera_position_l_button, back_to_menu_button, camera_position_title, camera_position_text]
+for y in settings_entities:
+    y.enabled = False
 
 main_menu()
 
